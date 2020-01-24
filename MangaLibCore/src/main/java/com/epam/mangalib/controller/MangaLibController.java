c7330321492af59be0f98af00fd3e0a0111ce42f
Andrey_Ivshin <Andrey_Ivshin@epam.com>
2020-01-24 10:28:45 +0600

    +core

--- MangaLibCore/src/main/java/com/epam/mangalib/controller/MangaLibController.java
+++ MangaLibCore/src/main/java/com/epam/mangalib/controller/MangaLibController.java
@@ -18,6 +18,8 @@ public class MangaLibController extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger ROOT_LOGGER = LogManager.getRootLogger();
 
+//    Hello
+
     @Override
     public void init() throws ServletException {
         super.init();
